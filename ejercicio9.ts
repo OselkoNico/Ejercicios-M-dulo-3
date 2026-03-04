c8c2d5f36c84056eb4842414bde5fb3652d9fa01
Osel_Nicolás <oselko123@gmail.com>
2026-03-04 16:34:33 +0100

    Ejercicio de tipados

---
+++ ejercicio9.ts
@@ -0,0 +1,13 @@
+type Usuario = {
+    id: number,
+    nombre: string,
+    email: string,
+    actividad: boolean
+};
+
+const u1: Usuario = {
+    id: 22,
+    nombre: "Osel",
+    email: "example@email.com",
+    actividad: true
+};
