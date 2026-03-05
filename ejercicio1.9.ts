a17b4e492cbffa99fd7f1802be1254914b51788f
Osel_Nicolás <oselko123@gmail.com>
2026-03-05 10:25:30 +0100

    Ejercicio 10

---
+++ ejercicio1.9.ts
@@ -0,0 +1,13 @@
+type Usuario = {
+    nombre: string,
+    edad: number,
+    activo: boolean
+}
+
+const n1: Usuario = {
+    nombre: "Osel",
+    edad: 29,
+    activo: true
+}
+
+console.log(n1);
