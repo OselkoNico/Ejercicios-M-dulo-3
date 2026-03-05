7c97cd9eb7125b56f11415bab931d8cb21244986
Osel_Nicolás <oselko123@gmail.com>
2026-03-05 10:29:04 +0100

    Ejercicio 10 de verdad

---
+++ ejercicio1.10.ts
@@ -0,0 +1,18 @@
+interface Persona {
+    nombre: string,
+    edad: number,
+    email: string
+}
+
+
+const p1: Persona = {
+    nombre: "Ana",
+    edad: 64,
+    email: "example@email.com"
+}
+
+const p2: Persona = {
+    nombre: "Juan",
+    edad: 45,
+    email: "example2@gmail.com"
+}
