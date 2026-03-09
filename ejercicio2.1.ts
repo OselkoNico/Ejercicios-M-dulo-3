c512db4258a9c3c90277073177e79cbce6a1cab7
Osel_Nicolás <oselko123@gmail.com>
2026-03-09 16:36:08 +0100

    2

---
+++ ejercicio2.1.ts
@@ -0,0 +1,19 @@
+type UserID = string | number;
+
+enum Role {
+    admin,
+    empleado,
+    trabajador
+}
+
+interface IUser {
+    id: number | string,
+    name: string,
+    age: number
+}
+
+class User {
+   constructor(id, name, age){
+    this.IUser.id,
+   }
+}
