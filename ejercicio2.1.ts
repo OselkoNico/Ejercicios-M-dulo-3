56434733a6725e20d590f9e59824ccaaa0a0c0d4
Osel_Nicolás <oselko123@gmail.com>
2026-03-10 11:04:23 +0100

    Ejercicio 1 de TS completado

--- ejercicio2.1.ts
+++ ejercicio2.1.ts
@@ -7,13 +7,44 @@ enum Role {
 }
 
 interface IUser {
-    id: number | string,
+    id: UserID,
     name: string,
     age: number
 }
 
-class User {
-   constructor(id, name, age){
-    this.IUser.id,
+class User implements IUser {
+    public id: UserID
+    public name: string
+    public age: number
+    protected rol: Role
+    private email: string
+    static userCount: number = 0
+   constructor(id: UserID, name: string, age: number, rol: Role, email: string){
+    this.id = id
+    this.name = name  
+    this.age = age
+    this.rol = rol
+    this.email = email
+    User.userCount++
    }
+   
+   getEmail(){
+    return this.email
+   }
+   setModificarEmail(nuevoEmail: string){
+    this.email = nuevoEmail
+   }
+   getRole(){
+    return this.rol
+   }
+}
+
+const users: User[] = [];
+
+const n1 = new User(12, "Osel", 30, Role.admin, "example@gmail.com");
+
+users.push(n1);
+
+function filtradoRoles(users: User[], role: Role): User[] {
+    return users.filter((user)=>user.getRole() === role)
 }
